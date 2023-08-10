f6da6c609bab92946f8dddf103225ec06b361217
Rafa <74217620+rafaalpizar@users.noreply.github.com>
2023-08-10 16:28:16 -0600

    Change zone to us-east-1a

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "blog_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "tf-blog-subnet"
