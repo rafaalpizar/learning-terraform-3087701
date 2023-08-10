4aeffcf8843f0aba0a6cf4ef06abd008ba7ae8dd
Rafa <74217620+rafaalpizar@users.noreply.github.com>
2023-08-10 15:55:45 -0600

    Adding VPC subnet and Instance interface

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -23,9 +22,33 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "blog_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-blog-subnet"
+  }
+}
+
+resource "aws_network_interface" "blog_netinterface" {
+  subnet_id   = aws_subnet.blog_subnet.id
+  private_ips = ["10.0.1.1"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  network_interface {
+    network_interface_id = aws_network_interface.blog_netinterface.id
+    device_index         = 0
+  }
 
   vpc_security_group_ids = [aws_security_group.blog.id]
 
